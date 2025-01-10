6accd92a9b80e7bcd23f39e756de74b41c3406eb
Dmytro Sobolev <damahmir@gmail.com>
2025-01-10 12:30:41 +0100

    lesson-18

---
+++ lesson-18/script.js
@@ -0,0 +1,51 @@
+"use strict";
+
+let a = 27;
+let b = 9;
+let c = 5;
+let d = 3;
+
+let result = a / b + c * d;
+console.log(result);
+
+
+
+let person1 = 'Anna';
+let person2 = 'Anfisa';
+let person3 = 'Gala';
+let person4 = 'Zhanna';
+let person5 = 'Diana';
+
+let persons = ['Anna', 'Anfisa', 'Gala', 'Zhanna', 'Diana']
+console.log(persons);
+
+let cooperatorName = 'Mike';
+let cooperatorEmail = 'mike@gmail.com';
+let cooperatorPassword = '1234567890';
+let cooperatorPhoneNumber = '+380112223344';
+
+let cooperator = {
+    name: 'Mike',
+    email: 'mike@gmail.com',
+    password: '1234567890',
+    phoneNumber: '+380112223344',
+}
+
+console.log(cooperatorPhoneNumber);
+
+let furniture1 = 'table';
+let furniture2 = 'chair';
+let furniture3 = 'armchair';
+let furniture4 = 'sofa';
+let furniture5 = 'closet';
+
+let furnitures = ['table', 'chair', 'armchair', 'sofa', 'closet'];
+console.log(furnitures);
+
+let jewerly1 = 'Gold';
+let jewerly2 = 'Silver';
+let jewerly3 = 'Diamond';
+let jewerly4 = 'Platinum';
+
+let jewerlys = ['Gold', 'Silver', 'Diamond', 'Platinum'];
+console.log(jewerlys);
